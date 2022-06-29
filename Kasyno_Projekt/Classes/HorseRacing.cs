85ac60d979ff729c5e7127c0c327c65bed72f20f
Rnbwf <Rnbwf@protonmail.com>
2022-06-29 21:06:09 +0200

    Basic HorseRacing Mechanic and animation
    Needs UI and Beting System

---
+++ Kasyno_Projekt/Classes/HorseRacing.cs
@@ -0,0 +1,68 @@
+﻿using Kasyno_Projekt.Classes;
+
+namespace Kasyno_Projekt.Classes
+{
+
+    public class Horse 
+    {
+        public string Id { get; set; }
+        public int Speed { get; set; }
+    
+        public Horse(string Id , int Speed)
+        {
+            this.Id = Id;
+            this.Speed = Speed;
+        }
+
+    }
+    public class HorseRacing
+    {
+        public Random rnd = new Random();
+        public List<Horse> Horses = new List<Horse>();
+        
+        
+        public void RaceHorses()
+        {
+            var Winner = rnd.Next(1, 11);
+
+            if (Winner < 6) 
+            {
+                var HorseToAdd = new Horse("R", rnd.Next(1, 6));
+                Horses.Add(HorseToAdd);
+
+                HorseToAdd = new Horse("B", rnd.Next(HorseToAdd.Speed + 1, 7));
+                Horses.Add(HorseToAdd);
+
+                HorseToAdd = new Horse("G", rnd.Next(HorseToAdd.Speed + 1, 8));
+                Horses.Add(HorseToAdd);
+                Horses=Horses.OrderBy(x => x.Id).ToList();
+
+            }
+            if (Winner >= 6 && Winner < 10)
+            {
+                var HorseToAdd = new Horse("B", rnd.Next(1, 6));
+                Horses.Add(HorseToAdd);
+
+                HorseToAdd = new Horse("R", rnd.Next(HorseToAdd.Speed + 1, 7));
+                Horses.Add(HorseToAdd);
+
+                HorseToAdd = new Horse("G", rnd.Next(HorseToAdd.Speed + 1, 8));
+                Horses.Add(HorseToAdd);
+                Horses = Horses.OrderBy(x => x.Id).ToList();
+            }
+            if (Winner == 10)
+            {
+                var HorseToAdd = new Horse("G", rnd.Next(1, 6));
+                Horses.Add(HorseToAdd);
+
+                HorseToAdd = new Horse("R", rnd.Next(HorseToAdd.Speed + 1, 7));
+                Horses.Add(HorseToAdd);
+
+                HorseToAdd = new Horse("B", rnd.Next(HorseToAdd.Speed + 1, 8));
+                Horses.Add(HorseToAdd);
+                Horses = Horses.OrderBy(x => x.Id).ToList();
+            }
+        }
+      
+    }
+}
---
+++ Kasyno_Projekt/Controllers/HorseRacingController.cs
@@ -0,0 +1,16 @@
+﻿using Kasyno_Projekt.Classes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kasyno_Projekt.Controllers
+{
+    public class HorseRacingController : Controller
+    {
+        public HorseRacing HorseRacingGame = new HorseRacing();
+
+        public IActionResult Index()
+        {
+            HorseRacingGame.RaceHorses();
+            return View(HorseRacingGame);
+        }
+    }
+}
